40d4b491dc5a926877220ca5ee27d281cd171023
Michael Pham <pducminh@search365.ai>
2024-03-07 00:45:56 +0700

    improve code

--- SPOSAPIDemo/APIHelper/Helper.cs
+++ SPOSAPIDemo/APIHelper/Helper.cs
@@ -37,113 +37,105 @@ namespace SPOSAPIDemo.API_Helper
         public static async Task<AddPaxRecordResponseModel> AddPax(string token, string trip_id, string voyage_date, PaxDetailsAdd[] pax_details)
         {
             string apiUrl = $"{Helper.BaseURL}/{"booking/add-pax-record"}";
-
-            using (HttpClient httpClient = new HttpClient())
+            try
             {
-                try
+                using (HttpClient client = new HttpClient())
                 {
-                    using (HttpClient client = new HttpClient())
+                    var data = new ResponseArray[] { };
+                    Array.Resize(ref pax_details, pax_details.Length - 1);
+                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                    var requestData = new
                     {
-                        var data = new ResponseArray[] { };
-                        Array.Resize(ref pax_details, pax_details.Length - 1);
-                        client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-                        var requestData = new
+                        trip_id,
+                        voyage_date,
+                        pax_details
+                    };
+
+                    string jsonRequestData = JsonConvert.SerializeObject(requestData);
+
+                    var content = new StringContent(jsonRequestData, Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonResponse = await response.Content.ReadAsStringAsync();
+                        JObject json = JObject.Parse(jsonResponse);
+                        ResponseArrayFull[] datares = JsonConvert.DeserializeObject<ResponseArrayFull[]>(json["data"].ToString());
+                        return new AddPaxRecordResponseModel
                         {
-                            trip_id,
-                            voyage_date,
-                            pax_details
+                            data = datares,
+                            err_msg = json["err_msg"].ToString(),
+                            err_num = json["err_num"].ToString()
                         };
-
-                        string jsonRequestData = JsonConvert.SerializeObject(requestData);
-
-                        var content = new StringContent(jsonRequestData, Encoding.UTF8, "application/json");
-                        HttpResponseMessage response = await client.PostAsync(apiUrl, content);
-
-                        if (response.IsSuccessStatusCode)
+                    }
+                    else
+                    {
+                        string error = await response.Content.ReadAsStringAsync();
+                        return new AddPaxRecordResponseModel
                         {
-                            string jsonResponse = await response.Content.ReadAsStringAsync();
-                            JObject json = JObject.Parse(jsonResponse);
-                            ResponseArrayFull[] datares = JsonConvert.DeserializeObject<ResponseArrayFull[]>(json["data"].ToString());
-                            return new AddPaxRecordResponseModel
-                            {
-                                data = datares,
-                                err_msg = json["err_msg"].ToString(),
-                                err_num = json["err_num"].ToString()
-                            };
-                        }
-                        else
-                        {
-                            string error = await response.Content.ReadAsStringAsync();
-                            return new AddPaxRecordResponseModel
-                            {
-                                error = error
-                            };
-                        }
+                            error = error
+                        };
                     }
                 }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return null;
-                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
             }
         }
 
         public static async Task<CancelPaxRecordResponseModel> CancelPax(string token, string trip_id, string voyage_date, string boarding_pass_number, int pax_id)
         {
             string apiUrl = $"{Helper.BaseURL}/{"booking/cancel-pax"}";
-            using (HttpClient httpClient = new HttpClient())
+            try
             {
-                try
+                using (HttpClient client = new HttpClient())
                 {
-                    var a = new object();
-                    using (HttpClient client = new HttpClient())
+                    PaxDetails details = new PaxDetails();
+                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                    var requestData = new
                     {
-                        PaxDetails details = new PaxDetails();
-                        client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-                        var requestData = new
+                        trip_id,
+                        voyage_date,
+                        boarding_pass_number,
+                        pax_id,
+                        pax_details = details
+                    };
+
+                    string jsonRequestData = Newtonsoft.Json.JsonConvert.SerializeObject(requestData);
+
+                    var content = new StringContent(jsonRequestData, Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonResponse = await response.Content.ReadAsStringAsync();
+                        JObject json = JObject.Parse(jsonResponse);
+
+                        ResponseArray[] datares = JsonConvert.DeserializeObject<ResponseArray[]>(json["data"].ToString());
+                        return new CancelPaxRecordResponseModel
                         {
-                            trip_id,
-                            voyage_date,
-                            boarding_pass_number,
-                            pax_id,
-                            pax_details = details
+                            data = datares,
+                            err_msg = json["err_msg"].ToString(),
+                            err_num = json["err_num"].ToString()
                         };
-
-                        string jsonRequestData = Newtonsoft.Json.JsonConvert.SerializeObject(requestData);
-
-                        var content = new StringContent(jsonRequestData, Encoding.UTF8, "application/json");
-                        HttpResponseMessage response = await client.PostAsync(apiUrl, content);
-
-                        if (response.IsSuccessStatusCode)
+                    }
+                    else
+                    {
+                        string error = await response.Content.ReadAsStringAsync();
+                        return new CancelPaxRecordResponseModel
                         {
-                            string jsonResponse = await response.Content.ReadAsStringAsync();
-                            JObject json = JObject.Parse(jsonResponse);
-
-                            ResponseArray[] datares = JsonConvert.DeserializeObject<ResponseArray[]>(json["data"].ToString());
-                            return new CancelPaxRecordResponseModel
-                            {
-                                data = datares,
-                                err_msg = json["err_msg"].ToString(),
-                                err_num = json["err_num"].ToString()
-                            };
-                        }
-                        else
-                        {
-                            string error = await response.Content.ReadAsStringAsync();
-                            return new CancelPaxRecordResponseModel
-                            {
-                                data = null,
-                                error = error
-                            };
-                        }
+                            data = null,
+                            error = error
+                        };
                     }
                 }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return null;
-                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
             }
         }
 
@@ -192,46 +184,42 @@ namespace SPOSAPIDemo.API_Helper
         public static async Task<GetPaxDetailResponseModel> GetPaxInfo(string token, string last_name, string voyage_date, string trip_id, string first_name, string passport_number, string dcs_booking_code, string customer_booking_code, string destination, string boarding_sequence, string include_cancel)
         {
             string apiUrl = $"{Helper.BaseURLdcs}/{"information/get-pax-info?last_name=" + last_name + "&voyage_date=" + voyage_date + "&trip_id=" + trip_id + "&first_name=" + first_name + "&passport_number=" + passport_number + "&dcs_booking_code=" + dcs_booking_code + "&customer_booking_code=" + customer_booking_code + "&destination=" + destination + "&boarding_sequence=" + boarding_sequence + "&include_cancel=" + include_cancel + ""}";
-
-            using (HttpClient httpClient = new HttpClient())
+            try
             {
-                try
+                using (HttpClient client = new HttpClient())
                 {
-                    using (HttpClient client = new HttpClient())
+
+                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
                     {
-
-                        client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-                        HttpResponseMessage response = await client.GetAsync(apiUrl);
-
-                        if (response.IsSuccessStatusCode)
+                        string jsonResponse = await response.Content.ReadAsStringAsync();
+                        JObject json = JObject.Parse(jsonResponse);
+                        Model.GetPaxList[] data = JsonConvert.DeserializeObject<Model.GetPaxList[]>(json["data"].ToString());
+                        return new GetPaxDetailResponseModel
                         {
-                            string jsonResponse = await response.Content.ReadAsStringAsync();
-                            JObject json = JObject.Parse(jsonResponse);
-                            Model.GetPaxList[] data = JsonConvert.DeserializeObject<Model.GetPaxList[]>(json["data"].ToString());
-                            return new GetPaxDetailResponseModel
-                            {
-                                data = data,
-                                err_msg = json["err_msg"].ToString(),
-                                err_num = json["err_num"].ToString()
-                            };
-                        }
-                        else
+                            data = data,
+                            err_msg = json["err_msg"].ToString(),
+                            err_num = json["err_num"].ToString()
+                        };
+                    }
+                    else
+                    {
+                        string error = await response.Content.ReadAsStringAsync();
+                        return new GetPaxDetailResponseModel
                         {
-                            string error = await response.Content.ReadAsStringAsync();
-                            return new GetPaxDetailResponseModel
-                            {
-                                error = error,
-                            };
-                        }
+                            error = error,
+                        };
                     }
                 }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return null;
-                }
-
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+
         }
 
     }
